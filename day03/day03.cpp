419f347c267300b2084cfd11d628dfda3d13833e
giperxiog <1512739072@qq.com>
2023-07-22 21:33:20 +0800

    day03 redo

--- day03/day03.cpp
+++ day03/day03.cpp
@@ -3,11 +3,15 @@
 //
 
 #include "day03.h"
-int DeleteMinRet(int a[], int n){
+int DeleteMinRet(int *a, int &n){
     int min = a[0], k = 0;
     for(int i = 1; i < n; i ++){
         if(a[i] < min) min = a[i], k = i;
     }
     a[k] = a[n - 1];
+    n--;
     return min;
 }
+
+
+### 代码
--- day03/day03.h
+++ day03/day03.h
@@ -5,14 +5,6 @@
 #ifndef ALGCAMP_DAY03_H
 #define ALGCAMP_DAY03_H
 
-int DeleteMinRet(int a[], int n);
-//int DeleteMinRet(int a[], int n){
-//    int min = a[0], k = 0;
-//    for(int i = 1; i < n; i ++){
-//        if(a[i] < min) min = a[i], k = i;
-//    }
-//    a[k] = a[n - 1];
-//    return min;
-//}
+int DeleteMinRet(int *a, int &n);
 
 #endif //ALGCAMP_DAY03_H
