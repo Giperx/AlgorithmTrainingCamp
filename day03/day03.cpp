9837f2471d117b575fbaec04463be6ad903a0b82
giperxiog <1512739072@qq.com>
2023-07-22 10:05:21 +0800

    day03

---
+++ day03/day03.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Giperx on 2023/7/22.
+//
+
+#include "day03.h"
+int DeleteMinRet(int a[], int n){
+    int min = a[0], k = 0;
+    for(int i = 1; i < n; i ++){
+        if(a[i] < min) min = a[i], k = i;
+    }
+    a[k] = a[n - 1];
+    return min;
+}
---
+++ day03/day03.h
@@ -0,0 +1,18 @@
+//
+// Created by Giperx on 2023/7/22.
+//
+
+#ifndef ALGCAMP_DAY03_H
+#define ALGCAMP_DAY03_H
+
+int DeleteMinRet(int a[], int n);
+//int DeleteMinRet(int a[], int n){
+//    int min = a[0], k = 0;
+//    for(int i = 1; i < n; i ++){
+//        if(a[i] < min) min = a[i], k = i;
+//    }
+//    a[k] = a[n - 1];
+//    return min;
+//}
+
+#endif //ALGCAMP_DAY03_H
