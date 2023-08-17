2cdf9f45ea29cdab0e386f95d47227f1fc1e551c
giperxiog <1512739072@qq.com>
2023-08-17 23:29:22 +0800

    add 三指针反转链表

--- week5/day25/day25.cpp
+++ week5/day25/day25.cpp
@@ -7,8 +7,25 @@
 using namespace std;
 
 
+// 递归反向输出链表
 void reversePrintLinkList(LinkList L){
     if (!L) return;
     reversePrintLinkList(L->next);
     cout << L->data << endl;
+}
+
+// 反转链表 三指针
+void reverseLinkList(LinkList &L){
+    LNode* pre = nullptr, *cur = L->next, *post = L->next;
+    while (cur){
+        //记录后一个结点
+        post = post->next;
+        //将当前结点反转
+        cur->next = pre;
+        //后移
+        pre = cur;
+        cur = post;
+    }
+    // 更新头结点所指一个结点
+    L->next = pre;
 }
--- week5/day25/day25.h
+++ week5/day25/day25.h
@@ -6,6 +6,10 @@
 #define ALGCAMP_DAY25_H
 #include "../../structDef/List/LinkList.h"
 
+// 递归反向输出链表
 void reversePrintLinkList(LinkList L);
+// 反转链表
+void reverseLinkList(LinkList &L);
+
 
 #endif //ALGCAMP_DAY25_H
